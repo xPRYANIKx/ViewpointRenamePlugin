d5143d60edf3bbfbebe234e88969b6451ff87bc3
Roman Tuleninov <tuleninov.rv@gmail.com>
2024-04-05 14:16:22 +0300

    Adding project files
    Adding project files

---
+++ Forms/RenameViewForm.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Autodesk.Navisworks.Api;
+using System.Windows;
+
+
+namespace PRYANIK_Plugin.Forms
+{
+    public partial class RenameViewForm : Window
+    {
+        Document doc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+        static string viewpointTextFloor, viewpointTextNumber;
+
+        public RenameViewForm()
+        {
+            InitializeComponent();
+            RecordingViewpointName();
+        }
+
+        internal void RenameViewPointObj()
+        {
+
+            var parent = doc.SavedViewpoints.CurrentSavedViewpoint.Parent;
+            int index = parent.Children.IndexOf(doc.SavedViewpoints.CurrentSavedViewpoint);
+
+            SavedItem newViewpoint = doc.SavedViewpoints.CurrentSavedViewpoint.CreateUniqueCopy();
+
+            newViewpoint.DisplayName = viewpointTextFloor + "." + viewpointTextNumber + "." + ViewPointTextName.Text;
+
+            if (int.TryParse(viewpointTextNumber, out int number))
+            {
+                viewpointTextNumber = (number + 1).ToString("000");
+            }
+
+            doc.SavedViewpoints.ReplaceWithCopy(parent, index, newViewpoint);
+        }
+
+        private void RecordingViewpointName()
+        {
+            ViewPointTextName.Text = doc.SavedViewpoints.CurrentSavedViewpoint.DisplayName;
+            ViewPointTextList.Text = doc.SavedViewpoints.CurrentSavedViewpoint.DisplayName;
+        }
+
+        private void Button_CloseClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_SaveClick(object sender, RoutedEventArgs e)
+        {
+            viewpointTextFloor = ViewPointTextFloor.Text;
+            viewpointTextNumber = ViewPointTextNumber.Text;
+            this.Close();
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,55 @@
+﻿using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+using PRYANIK_Plugin.Forms;
+
+namespace PRYANIK_Plugin
+{
+    [Plugin("PRYANIK_Plugin", "хPRYANIKх", DisplayName = "PRYANIK_Plugin")]
+    [RibbonLayout("PRYANIK_Plugin.xaml")]
+
+    [RibbonTab("PluginTab", DisplayName = "PRYANIK_Plugin")]
+    [Command("ID_Button_A", DisplayName = "Настройка для пакетного переименования точек обзора", Icon = "Source\\rename_small.png", LargeIcon = "Source\\rename_big.png", 
+        ToolTip = "Настройка для пакетного переименования точек обзора.\nДля переименования точки обзора - задайте параметры в данном окне, после чего выберите необходимую точку обзора и " +
+        "нажмите клавишу Q", CanToggle = true)]
+    public class Main : CommandHandlerPlugin
+    {
+        public override int ExecuteCommand(string name, params string[] parameters)
+        {
+            switch (name)
+            {
+                case "ID_Button_A":
+                    {
+                        if (Application.ActiveDocument?.SavedViewpoints?.CurrentSavedViewpoint?.DisplayName != null)
+                        {
+                            RenameViewForm renameViewForm = new RenameViewForm();
+                            renameViewForm.ShowDialog();
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+            return 0;
+        }
+    }
+}
+
+namespace PRYANIK_Plugin_inputPlugin
+{
+    [Plugin("PRYANIK_inputPlugin", "хPRYANIKх")]
+    public class Main : InputPlugin
+    {
+        public override bool KeyUp(View view, KeyModifiers modifier, ushort key, double timeOffset)
+        {
+            if (Application.ActiveDocument?.SavedViewpoints?.CurrentSavedViewpoint?.DisplayName != null && key == 81)
+            {
+                RenameViewForm renameViewForm = new RenameViewForm();
+                renameViewForm.RenameViewPointObj();
+            }
+
+            return base.KeyUp(view, modifier, key, timeOffset);
+        }
+    }
+}
